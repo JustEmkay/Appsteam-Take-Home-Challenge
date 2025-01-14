65c435c35c556e1e5c7b3fcaca838101659eb3b3
JustEmkay <just.mk.things@gmail.com>
2025-01-14 12:54:50 +0530

    removed files

--- test-files/Scrape.py
+++
@@ -1,8 +0,0 @@
-from newspaper import Article
-
-url = 'https://www.bbc.co.uk/news/articles/cdekew421dgo'
-article = Article(url)
-article.download()
-article.parse()
-s =article.text
-print(s)
--- test-files/newApi.py
+++
@@ -1,9 +0,0 @@
-import reqs
-from pprint import pprint
-
-
-url = ('https://newsapi.org/v2/top-headlines?'
-       'sources=bbc-news&'
-       'apiKey=2bf69752e89045b6a4ace9ab036c4b85')
-response = reqs.get(url)
-pprint (response.json())
